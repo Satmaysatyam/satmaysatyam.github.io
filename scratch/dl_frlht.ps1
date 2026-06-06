541d7e8c7ad74579ca973b3ee9d40b1522f8bad2
Satyam Gupta <satmaysatyam@gmail.com>
2026-06-06 17:50:04 +0530

    style: remove state government logos from PBR projects

---
+++ scratch/dl_frlht.ps1
@@ -0,0 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://static.wixstatic.com/media/e9256b_62de9273c73c4ae0a385a3584966d781~mv2.png/v1/fill/w_246,h_150,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/e9256b_62de9273c73c4ae0a385a3584966d781~mv2.png" -OutFile "c:\Users\satma\Documents\Personal Website\logos\frlht.png"
